a26ff220b65e04fd73f35b84fec0e5821f86237f
Mikołaj Rosowski <m.rosowski1@wp.pl>
2020-04-20 21:54:11 +0200

    Fixed search_case_insensitive to correctly match

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     contents
         .lines()
-        .filter(|line| line.contains(&query))
+        .filter(|line| line.to_lowercase().contains(&query))
         .collect()
 }
 
